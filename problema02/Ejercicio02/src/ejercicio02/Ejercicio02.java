24cbbb765db56e810103d76e70dc952bce3e8445
Javier Vinan <javinan5@utpl.edu.ec>
2024-11-14 12:56:26 -0500

    problema02

---
+++ problema02/Ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio02;
+import java.util.Scanner;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        String tipo;
+        String nombre;
+        double vehiculo;
+        double peaje;
+        System.out.println("Ingrese sus nombres:");
+        nombre = entrada.nextLine();
+        System.out.println("Ingrese el valor del vehiculo:");
+        vehiculo = entrada.nextDouble();
+        
+        switch(tipo){
+            
+        }
+    }
+    
+}
