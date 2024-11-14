c8d48fa717066a588392f19280a4dc8db1eb7fca
Javier Vinan <javinan5@utpl.edu.ec>
2024-11-14 12:40:09 -0500

    problema01

---
+++ problema01/Ejercicio01/src/ejercicio01/Ejercicio01.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio01;
+import java.util.Scanner;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double num1;
+        double num2;
+        double resultado;
+        resultado = 0;
+        String operacion;
+        System.out.println("Ingrese el numero 1:");
+        num1 = entrada.nextDouble();
+        System.out.println("Ingrese el numero 2:");
+        num2 = entrada.nextDouble();
+        entrada.nextLine();
+        System.out.println("Que operacion desea ejecutar:");
+        operacion = entrada.nextLine();
+        operacion = operacion.toLowerCase();
+        
+        if (num1 > num2){
+            switch(operacion){
+                case "suma":
+                    resultado = num1 + num2;
+                break;
+                case "resta":
+                    resultado = num1 - num2;
+                break;
+                case "multiplicacion":
+                    resultado = num1 * num2;
+                break;
+                case "division":
+                    resultado = num1/num2;
+                break;
+                case "modulo":
+                    resultado = num1 % num2;
+                break;
+                case "potencia":
+                    resultado = Math.pow(num1, num2);
+                break;   
+                
+            }
+            System.out.printf("El resultado es: %.2f\n", resultado);
+        }else{
+            System.out.printf("El resultado no es posible\n");
+        }
+            
+    }
+}
+        
+       
+
+    
+
